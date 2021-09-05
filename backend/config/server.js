5c0637fab6d82cdb7b56d62d44c98563ded0d890
chagy <chagy_x_@hotmail.com>
2021-09-05 14:21:45 +0700

    Backend 35

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d2ba81315b16fe2602ac5af025d0b23'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1424632a-168d-4cb7-b696-b29d5d5084b6'
+};
