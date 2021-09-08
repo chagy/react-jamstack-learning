baa050e1401c6056ec739e3d9d814ec659fc9496
chagy <chagy_x_@hotmail.com>
2021-09-08 08:44:23 +0700

    57

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ["Philosopher:i7:latin", "Montserrat:n5,n4,n3:latin"],
+          families: ["Philosopher:i7:latin", "Montserrat:n6,n5,n4,n3:latin"],
         },
       },
     },
@@ -34,18 +34,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
@@ -6,33 +6,144 @@ import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
+import {
+  useMediaQuery,
+  Hidden,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core"
+import { makeStyles } from "@material-ui/core"
+import { Link, navigate } from "gatsby"
 
 import search from "../../images/search.svg"
 import cart from "../../images/cart.svg"
 import account from "../../images/account-header.svg"
+import menu from "../../images/menu.svg"
 
-export default function Header() {
+const useStyles = makeStyles(theme => ({
+  colorIndecator: {
+    backgroundColor: "#fff",
+  },
+  logoText: {
+    color: theme.palette.common.black,
+  },
+  logoContainer: {
+    [theme.breakpoints.down("md")]: {
+      marginRight: "auto",
+    },
+  },
+  tab: {
+    ...theme.typography.body1,
+    fontWeight: 600,
+  },
+  tabs: {
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  icon: {
+    height: "3rem",
+    width: "3rem",
+  },
+  drawer: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  listItemText: {
+    color: "#FFF",
+  },
+}))
+
+export default function Header({ categories }) {
+  const classes = useStyles()
+  const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
+
+  const [drawerOpen, setDrawerOpen] = useState(false)
+
+  // const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent)
+
+  const routes = [
+    ...categories,
+    { node: { name: "Contact Us", strapiId: "contactus", link: "/contact" } },
+  ]
+
+  const tabs = (
+    <Tabs
+      value={0}
+      classes={{
+        indicator: classes.colorIndecator,
+        root: classes.tabs,
+      }}
+    >
+      {routes.map(route => (
+        <Tab
+          component={Link}
+          to={route.node.link || `/${route.node.name.toLowerCase()}`}
+          classes={{ root: classes.tab }}
+          label={route.node.name}
+          key={route.node.strapiId}
+        />
+      ))}
+    </Tabs>
+  )
+
+  const drawer = (
+    <SwipeableDrawer
+      open={drawerOpen}
+      onOpen={() => setDrawerOpen(true)}
+      onClose={() => setDrawerOpen(false)}
+      // disableBackdropTransition={!iOS}
+      // disableDiscovery={iOS}
+      classes={{ paper: classes.drawer }}
+    >
+      <List disablePadding>
+        {routes.map(route => (
+          <ListItem divider button key={route.node.strapiId}>
+            <ListItemText
+              classes={{ primary: classes.listItemText }}
+              primary={route.node.name}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </SwipeableDrawer>
+  )
+
+  const actions = [
+    { icon: search, alt: "search", visible: true },
+    { icon: cart, alt: "cart", visible: true, link: "/cart" },
+    { icon: account, alt: "account", visible: !matchesMD, link: "/account" },
+    {
+      icon: menu,
+      alt: "menu",
+      visible: matchesMD,
+      onClick: () => setDrawerOpen(true),
+    },
+  ]
+
   return (
     <AppBar color="transparent" elevation={0}>
       <Toolbar>
-        <Button>
-          <Typography variant="h1">VAR X</Typography>
+        <Button classes={{ root: classes.logoContainer }}>
+          <Typography variant="h1">
+            <span className={classes.logoText}>VAR</span> X
+          </Typography>
         </Button>
-        <Tabs>
-          <Tab label="Hats" />
-          <Tab label="Hoodies" />
-          <Tab label="Shirts" />
-          <Tab label="Contact Us" />
-        </Tabs>
-        <IconButton>
-          <img src={search} alt="search" />
-        </IconButton>
-        <IconButton>
-          <img src={cart} alt="cart" />
-        </IconButton>
-        <IconButton>
-          <img src={account} alt="account" />
-        </IconButton>
+        {matchesMD ? drawer : tabs}
+        {actions.map(action => {
+          if (action.visible) {
+            return (
+              <IconButton component={Link} to={action.link} key={action.alt}>
+                <img
+                  className={classes.icon}
+                  src={action.icon}
+                  alt={action.alt}
+                  onClick={action.onClick}
+                />
+              </IconButton>
+            )
+          }
+        })}
       </Toolbar>
     </AppBar>
   )
